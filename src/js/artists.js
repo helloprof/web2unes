9c19efb5820ebed06b2a76107b2653d14e35cd22
helloprof <suluxan.seneca@gmail.com>
2023-10-18 10:05:47 -0400

    example list of songs and artists for our app

---
+++ src/js/artists.js
@@ -0,0 +1,44 @@
+/**
+ * artists.js
+ *
+ * The app's list of Artists
+ */
+
+window.artists = [
+  {
+    artistID: 1,
+    name: "Drake",
+    links: [
+      {
+        image: ""
+      }    
+    ]
+  },
+  {
+    artistID: 2,
+    name: "Eminem",
+    links: [
+      {
+        image: ""
+      }    
+    ]
+  },
+  {
+    artistID: 3,
+    name: "Taylor Swift",
+    links: [
+      {
+        image: ""
+      }    
+    ]
+  },
+  {
+    artistID: 4,
+    name: "Doja Cat",
+    links: [
+      {
+        image: ""
+      }    
+    ]
+  },
+];
---
+++ src/js/songs.js
@@ -0,0 +1,59 @@
+/**
+ * songs.js
+ *
+ * The app's songs
+ */
+
+window.songs = [
+  {
+    songID: 1, // primary key
+    artistID: 4, // foreign key
+    title: "Paint The Town Red",
+    albumCover: "https://is1-ssl.mzstatic.com/image/thumb/Music126/v4/05/a1/62/05a162ca-d0f8-462a-a033-474a019e506f/196871274111.jpg/600x600bb.jpg",
+    audio: "url",
+    explicit: true,
+    albumName: "Scarlet",
+    duration: "3:51",
+  },
+  {
+    songID: 2, // primary key
+    artistID: 4, // foreign key
+    title: "Skull and Bones",
+    albumCover: "https://is1-ssl.mzstatic.com/image/thumb/Music126/v4/05/a1/62/05a162ca-d0f8-462a-a033-474a019e506f/196871274111.jpg/600x600bb.jpg",
+    audio: "url",
+    explicit: true,
+    albumName: "Scarlet",
+    duration: "4:08",
+  },
+  {
+    songID: 3, // primary key
+    artistID: 4, // foreign key
+    title: "Love Life",
+    albumCover: "https://is1-ssl.mzstatic.com/image/thumb/Music126/v4/05/a1/62/05a162ca-d0f8-462a-a033-474a019e506f/196871274111.jpg/600x600bb.jpg",
+    audio: "url",
+    explicit: true,
+    albumName: "Scarlet",
+    duration: "3:56",
+  },
+  {
+    songID: 4, // primary key
+    artistID: 1, // foreign key
+    title: "Away From Home",
+    albumCover: "https://is1-ssl.mzstatic.com/image/thumb/Music116/v4/21/50/ee/2150ee84-62c3-4190-7dfa-da30abd98ac8/23UM1IM09862.rgb.jpg/600x600bb.jpg",
+    audio: "url",
+    explicit: true,
+    albumName: "For All The Dogs",
+    duration: "4:18",
+  },
+  {
+    songID: 5, // primary key
+    artistID: 1, // foreign key
+    title: "First Person Shooter",
+    albumCover: "https://is1-ssl.mzstatic.com/image/thumb/Music116/v4/21/50/ee/2150ee84-62c3-4190-7dfa-da30abd98ac8/23UM1IM09862.rgb.jpg/600x600bb.jpg",
+    audio: "url",
+    // features: "J. Cole",
+    explicit: true,
+    albumName: "For All The Dogs",
+    duration: "4:18",
+  },
+];
