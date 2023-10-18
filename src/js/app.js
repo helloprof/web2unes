77911f6ac6e2033f3afdf5781792effd9488c2da
helloprof <suluxan.seneca@gmail.com>
2023-10-18 10:06:05 -0400

    create artist buttons for array of artists and set up for onLoad and onClick events

---
+++ src/js/app.js
@@ -0,0 +1,46 @@
+/**
+ * WEB222 – Assignment 04
+ *
+ * I declare that this assignment is my own work in accordance with
+ * Seneca Academic Policy. No part of this assignment has been
+ * copied manually or electronically from any other source
+ * (including web sites) or distributed to other students.
+ *
+ * Please update the following with your information:
+ *
+ *      Name:       <YOUR_NAME>
+ *      Student ID: <YOUR_STUDENT_ID>
+ *      Date:       <SUBMISSION_DATE>
+ */
+
+// All of our data is available on the global `window` object.
+// Create local variables to work with it in this file.
+const { artists, songs } = window;
+
+// For debugging, display all of our data in the console. You can remove this later.
+console.log({ artists, songs }, "App Data");
+
+function listSongs(artistID) {
+
+}
+
+function listArtists() {
+    console.log("listing artists")
+    const menu = document.getElementById("menu")
+    artists.forEach((artist) => {
+        const button = document.createElement("button")
+        button.className = "button"
+        button.textContent = artist.name
+        menu.appendChild(button)
+        button.addEventListener("click", (e) => {
+            listSongs(artist.artistID)
+        })
+    })
+}
+
+function onLoad() {
+    console.log("loaded")
+    listArtists()
+}
+
+window.addEventListener("load", onLoad)
