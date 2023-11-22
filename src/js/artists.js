41cdcc55f580881067e80cd37088387e65ccb9d8
helloprof <suluxan.seneca@gmail.com>
2023-11-22 10:00:14 -0500

    adding image data to object

--- src/js/artists.js
+++ src/js/artists.js
@@ -8,6 +8,7 @@ window.artists = [
   {
     artistID: 1,
     name: "Drake",
+    image: "https://i.scdn.co/image/ab6761610000e5eb4293385d324db8558179afd9",
     links: [
       {
         image: ""
@@ -17,6 +18,7 @@ window.artists = [
   {
     artistID: 2,
     name: "Eminem",
+    image: "https://i.scdn.co/image/ab6761610000e5eba00b11c129b27a88fc72f36b",
     links: [
       {
         image: ""
@@ -26,6 +28,7 @@ window.artists = [
   {
     artistID: 3,
     name: "Taylor Swift",
+    image: "https://i.scdn.co/image/ab67616d00001e020b04da4f224b51ff86e0a481",
     links: [
       {
         image: ""
@@ -35,6 +38,7 @@ window.artists = [
   {
     artistID: 4,
     name: "Doja Cat",
+    image: "https://i.scdn.co/image/ab67706c0000da84aa155a00de8e8aef811c2f82",
     links: [
       {
         image: ""
